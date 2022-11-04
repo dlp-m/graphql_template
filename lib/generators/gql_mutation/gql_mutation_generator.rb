37e35129ddee5f84c00b922ff7cbe0b36172cc2e
Tyflomate <florian@tymate.com>
2022-11-04 14:09:44 +0100

    feat: update mutation type with new mutation in generator

--- lib/generators/gql_mutation/gql_mutation_generator.rb
+++ lib/generators/gql_mutation/gql_mutation_generator.rb
@@ -3,7 +3,16 @@ class GqlMutationGenerator < Rails::Generators::NamedBase
 
   def create_gql_file
     template 'exemple_mutation.rb', File.join('app/graphql/mutations', "#{file_name.underscore}.rb")
-    template 'exemple_mutation_spec.rb', File.join('spec/graphql/mutations', "#{file_name.underscore}s_spec.rb")
+    template 'exemple_mutation_spec.rb', File.join('spec/graphql/mutations', "#{file_name.underscore}_spec.rb")
     template 'exemple_mutation.graphql.rb', File.join('spec/fixtures/graphql/mutations/', "#{file_name.underscore}s.graphql")
+    update_mutation_type
+  end
+
+  private
+
+  def update_mutation_type
+    path = 'app/graphql/types/mutation_type.rb'
+    mutations = File.read(path).gsub("end\nend\n", "  field :test_mutation, mutation: Mutations::#{file_name.camelize}\n  end\nend\n")
+    File.write(path, mutations)
   end
 end
