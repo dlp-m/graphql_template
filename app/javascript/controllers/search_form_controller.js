55f80a4e723cff601654468e882ab649a3f792a6
dlp-m <michel@tymate.com>
2022-11-07 09:22:34 +0100

    chore: code style

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["form"]
+
   search() {
     clearTimeout(this.timeout)
     this.timeout = setTimeout(() => {
