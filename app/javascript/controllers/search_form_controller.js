fb5aaa02d3820b8744f2ec60579c6d9db84a5b0f
dlp-m <michel@tymate.com>
2022-11-07 10:33:07 +0100

    feat: fix javascript filter for boolean

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -14,6 +14,7 @@ export default class extends Controller {
     let value = e.target.getAttribute('data-value')
     let targetId = e.target.getAttribute('data-target-id')
     let target = document.getElementById(targetId)
+    target.value = value
     this.formTarget.requestSubmit()
   }
 }
