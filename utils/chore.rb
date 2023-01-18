1b85374a8e49bb2721d41395f6159b6c2643891a
dlp-m <michel@tymate.com>
2023-01-18 14:52:47 +0100

    feat: add mail configuration and add some fixes

--- utils/chore.rb
+++ utils/chore.rb
@@ -1,7 +1,5 @@
 def chore
   custom_log(__method__)
-  system "touch .env; touch .env.exemple"
-  system "echo .env >> .gitignore"
   system "echo coverage/**/* >> .gitignore"
   system "git add . ; git commit -m 'chore: update gitignore'"
   system "bundle exec rubocop -A"
--- utils/rspec.rb
+++ utils/rspec.rb
@@ -2,7 +2,10 @@ def configure_rspec
   custom_log(__method__)
   system "rails generate rspec:install"
   remove_dir "test"
-  create_or_replace_folders(Dir["#{source_paths.first}/spec/*"])
+  create_or_replace_folders(
+    files: Dir["#{source_paths.first}/spec/*"],
+    exclude_files: ['spec/graphql/queries/frequently_asked_questions_spec.rb']
+  )
   system "bundle exec rubocop -A"
   system "bundle exec rails db:migrate RAILS_ENV=test"
   system "git add . ; git commit -m 'feat: setup rspec'"
