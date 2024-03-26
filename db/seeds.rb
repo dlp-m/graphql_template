7fe10cb36c76971daa6f2b65f34083d2099e1a0b
dlp-m <michel@tymate.com>
2024-03-26 08:45:23 +0100

    feat: update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@
 require_relative 'seeds/users'
 
 if Rails.env.development?
-  Doorkeeper::Application.find_or_create_by!(
-  name: 'front-app',
-  uid: 'tGhT2JBVw1dqUtNSJf1Uibu1c8MnMpd5LXXFKSsVLq8',
-  secret: '5EYlm_7CByaoSeSk2XX7rOaOdq_BSwihb55ukCNHVCo',
-  redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-  confidential: false
-)
+#   Doorkeeper::Application.find_or_create_by!(
+#   name: 'front-app',
+#   uid: '',
+#   secret: '',
+#   redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+#   confidential: false
+# )
 end
